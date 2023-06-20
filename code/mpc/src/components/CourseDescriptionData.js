fa29a2b7d9b5b9c84debb80b3fc403cc3feecc37
Justin Dickerson <21042279+dj911ice@users.noreply.github.com>
2023-06-20 16:28:11 -0400

    added additional padding and line breaks

--- code/mpc/src/components/CourseDescriptionData.js
+++ code/mpc/src/components/CourseDescriptionData.js
@@ -4,7 +4,9 @@ function CourseDescriptionData({course}) {
     return (
         <dt className={"courseDT"}>
             <dd> {course.courseIdentifier}<span>: </span>{course.courseName}</dd>
+            <br/>
             <dd>{course.courseDescription}</dd>
+            <br/>
             <dd><span>Course Issuer: </span>{course.courseIssuer}</dd>
             <dd><span>Status: </span>{course.courseCompleted === true ? "Completed": "In Progress"}</dd>
             <br/>
