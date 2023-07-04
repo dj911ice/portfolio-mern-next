6465d416f3b3723137721bfee36bf6cfb1c926b0
Justin Dickerson <21042279+dj911ice@users.noreply.github.com>
2023-07-03 21:06:42 -0400

    Added Material Tailwind carousel to projects page.

--- code/mpc/src/pages/projects.js
+++ code/mpc/src/pages/projects.js
@@ -1,67 +1,170 @@
-import React from "react";
 import Header from "@/components/Header";
-import ImageGallery from 'react-image-gallery';
-
-const projects = [
-    {
-        description: 'Dance Products: Ruby on Rails Ecommerce portfolio project ',
-        original: '/images/mytestappjpd-sm.png',
-        thumbnail: '/images/mytestappjpd-sm.png',
-        embedUrl: 'https://youtu.be/PQXwOXdSj9U',
-        originalWidth: '1024px',
-        originalHeight: '640px'
-    },
-
-    {
-        description: 'Fizz Buzz Interactive: Output to popular Fizz Buzz problem but interactive written in Ruby',
-        original: '/images/fbi_ruby.png',
-        thumbnail: '/images/fbi_ruby.png',
-        originalWidth: '1024px',
-        originalHeight: '640px'
-    },
-
-    {
-        description: 'Habitica Abroad: Task webapp in C# & .Net framework',
-        original: '/images/HabiticaAbroad_dashboad.PNG',
-        thumbnail: '/images/HabiticaAbroad_dashboad.PNG',
-        originalWidth: '1024px',
-        originalHeight: '640px'
-    },
-
-    {
-        description: 'Grand Circus Library: Console application written C#',
-        original: '/images/GCLibrary_sc.PNG',
-        thumbnail: '/images/GCLibrary_sc.PNG',
-        originalWidth: '1024px',
-        originalHeight: '640px'
-    },
-
-    {
-        description: 'JD Music Player: Android mobile application that plays music via streaming url or local file',
-        original: '/images/exoplayer_audio.png',
-        thumbnail: '/images/exoplayer_audio.png',
-        originalWidth: '600px',
-        originalHeight: '1200px'
-    }
-]
+import {Button, Carousel, Typography} from "@material-tailwind/react";
 
 function Projects() {
+
     return (
         <>
             <Header/>
             <main>
                 <section>
                     <h2>Awesome Projects</h2>
-                        <ImageGallery
-                            items={projects}
-                            autoPlay={true}
-                            showThumbnails={false}
-                        />
-                        {/*{projects.map((link, linkIndex) => {*/}
-                        {/*    return (*/}
-                        {/*        <a key={linkIndex} href={link.embedUrl} target={"_blank"}></a>*/}
-                        {/*    )*/}
-                        {/*})}*/}
+                    <div>
+                        <Carousel
+                            transition={{ duration: 1.5 }}
+                            loop={true}
+                            autoplay={true}
+                            autoplayDelay={3500}
+                            className="rounded-xl">
+                            <div className="relative h-full w-full">
+                                <img
+                                    src="/images/mytestappjpd-sm.png"
+                                    alt="Dance Products"
+                                    className="h-full w-full object-cover"
+                                />
+                                <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                                    <div className="w-3/4 text-center md:w-2/4">
+                                        <Typography
+                                            variant="h1"
+                                            color="white"
+                                            className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                                        >
+                                            Dance Products eCommerce Application
+                                        </Typography>
+                                        <Typography
+                                            variant="lead"
+                                            color="white"
+                                            className="mb-12 opacity-80"
+                                        >
+                                            Ruby on Rails portfolio project
+                                        </Typography>
+                                        <div className="flex justify-center gap-2">
+                                            <Button size="lg" color="white"
+                                                    onClick={event =>
+                                                        window.open("https://youtu.be/PQXwOXdSj9U", "_blank")}
+                                            >
+                                                Screencast
+                                            </Button>
+                                            <Button size="lg" color="white" variant="text"
+                                                    onClick={event =>
+                                                        window.open("https://github.com/dj911ice/nameofapp", "_blank")}
+                                            >
+                                                Code
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="relative h-full w-full">
+                                <img
+                                    src="/images/fbi_ruby.png"
+                                    alt="Fizz Buzz Interactive in Ruby"
+                                    className="h-full w-full object-cover"
+                                />
+                                <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+                                    <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+                                        <Typography
+                                            variant="h1"
+                                            color="white"
+                                            className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                                        >
+                                            Fizz Buzz Interactive
+                                        </Typography>
+                                        <Typography
+                                            variant="lead"
+                                            color="white"
+                                            className="mb-12 opacity-80"
+                                        >
+                                            Output to popular Fizz Buzz problem but interactive written in Ruby.
+                                        </Typography>
+                                        <div className="flex gap-2">
+                                            <Button size="lg" color="white"
+                                                    onClick={event =>
+                                                        window.open("https://www.youtube.com/watch?v=QkaiDw4WEoM", "_blank")}
+                                            >
+                                                Screencast
+                                            </Button>
+                                            <Button size="lg" color="white" variant="text" disabled={true}>
+                                                Download: Coming Soon!
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="relative h-full w-full">
+                                <img
+                                    src="/images/HabiticaAbroad_dashboad.PNG"
+                                    alt="Habitica Abroad: Task webapp in C# & .Net framework"
+                                    className="h-full w-full object-cover"
+                                />
+                                <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+                                    <div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
+                                        <Typography
+                                            variant="h1"
+                                            color="white"
+                                            className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                                        >
+                                            Habitica Abroad
+                                        </Typography>
+                                        <Typography
+                                            variant="lead"
+                                            color="white"
+                                            className="mb-12 opacity-80"
+                                        >
+                                            Task webapp in C# & .Net framework
+                                        </Typography>
+                                        <div className="flex gap-2">
+                                            <Button size="lg" color="white"
+                                                    onClick={event =>
+                                                        window.open("https://youtu.be/eyWo9FHxzB4", "_blank")}
+                                            >
+                                                Screencast
+                                            </Button>
+                                            <Button size="lg" color="white" variant="text" disabled={true}>
+                                                Code
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="relative h-full w-full">
+                                <img
+                                    src="/images/GCLibrary_sc.PNG"
+                                    alt="Grand Circus Library"
+                                    className="h-full w-full object-cover"
+                                />
+                                <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+                                    <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+                                        <Typography
+                                            variant="h1"
+                                            color="white"
+                                            className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                                        >
+                                            Grand Circus Library
+                                        </Typography>
+                                        <Typography
+                                            variant="lead"
+                                            color="white"
+                                            className="mb-12 opacity-80"
+                                        >
+                                            Console application written C#
+                                        </Typography>
+                                        <div className="flex gap-2">
+                                            <Button size="lg" color="white"
+                                                    onClick={event =>
+                                                        window.open("https://youtu.be/FdY97Lx7Vo4", "_blank")}
+                                            >
+                                                Screencast
+                                            </Button>
+                                            <Button size="lg" color="white" variant="text" disabled={true}>
+                                                Code
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Carousel>
+                    </div>
                 </section>
             </main>
             <footer>
---
+++ code/mpc/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT(
+    {
+        // prefix: 'tw-',
+        content: [
+            // "./app/**/*.{js,ts,jsx,tsx,mdx}",
+            // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+            // "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+            // Or if using `src` directory:
+            "./src/**/*.{js,ts,jsx,tsx,mdx}",
+            // "./src/pages/projects.{js,ts,jsx,tsx,mdx}",
+        ],
+        corePlugins: {
+            preflight: false,
+        },
+        theme: {
+            extend: {},
+        },
+        plugins: [],
+    }
+)
