4d42f4dea7be4719b99567e6f040b588a053748e
Justin Dickerson <21042279+dj911ice@users.noreply.github.com>
2023-12-14 22:27:32 -0500

    added conditional when credential link(s) return null

--- code/mpc/src/components/CredentialRow.js
+++ code/mpc/src/components/CredentialRow.js
@@ -6,7 +6,11 @@ function CredentialRow({credential}) {
         <tr>
             <td>{credential.credentialType}</td>
             <td>{credential.credentialLevel}</td>
-            <td>{credential.credentialField}</td>
+            <td>
+                {credential.credentialLink === null ?
+                    credential.credentialField :
+                    <Link href={credential.credentialLink} target={"_blank"}>{credential.credentialField}</Link>}
+            </td>
             <td>
                 {credential.institutionalLink === null ?
                     credential.credentialCertifier :
